d980aeedcd66c1b463100ffa409de3dddbd3b947
ramizhk <ramizhk.7042@gmail.com>
2026-07-07 18:45:24 +0500

    Initial commit: Flask coin flipper app working

---
+++ app.py
@@ -0,0 +1,25 @@
+import random
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    image_url = None
+
+    if request.method == "POST":
+        outcome = random.choice(["Heads", "Tails"])
+        result = f"It's {outcome}!"
+        # Python seekhni paray gi sahi se
+
+        if outcome == "Heads":
+            image_url = "/static/heads.jpg"
+        else:
+            image_url = (
+                "/static/tails.jpg"
+            )
+    return render_template("index.html", result=result, image_url=image_url)
+
+if __name__ == "__main__":
+    app.run(debug=True)
